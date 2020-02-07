797629272195b9dc1e91324af2649b6e1dee982b
Сергей <sag163@mail.ru>
2020-02-07 11:00:29 +0400

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import telebot;
+ 
+
+ 
+
+
+
+bot = telebot.TeleBot('642376706:AAFertFa44xSJ8ErRyqCVMAjmehUAYhSxwg')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
+
+
